e64f7c76d06f1c221761dd66b29064f854dfc4e7
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2023-04-10 15:00:58 +0200

    fix: og

--- src/components/PageHead.tsx
+++ src/components/PageHead.tsx
@@ -16,31 +16,10 @@ export function PageHead({
   return (
     <Head>
       <title>{title}</title>
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/img/icons/apple-touch-icon.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/img/icons/favicon-32x32.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/img/icons/favicon-16x16.png"
-      />
       <meta name="description" content={description} />
       <meta name="keywords" content={keywords?.join()} />
-
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
-      <meta property="og:url" content="https://berente.net/" />
-      <meta property="og:image" content="/img/banner.png" />
-      <link rel="manifest" href="/site.webmanifest" />
       {children}
     </Head>
   );
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,31 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/img/icons/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/img/icons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/img/icons/favicon-16x16.png"
+        />
+
+        <meta property="og:url" content="https://berente.net/" />
+        <meta property="og:image" content="/img/banner.png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
       <body>
         <Main />
         <NextScript />
